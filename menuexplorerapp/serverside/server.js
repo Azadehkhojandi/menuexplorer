5605e5123b7ac9b57e6fd58c3ae2e7f883d1c886
Azadeh Khojandi <azadehkhojandi@gmail.com>
2018-01-04 16:08:15 +1100

    update assests server.js

--- menuexplorerapp/serverside/server.js
+++ menuexplorerapp/serverside/server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 var path = require("path");
 
-app.use('/assets', express.static('assets'));
+app.use('/assets', express.static(__dirname +'./../assets'));
 
 app.get('/', function (req, res) {
   let tmp = path.join(__dirname + './../index.html');
